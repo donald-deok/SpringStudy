fc499ad105c7ddfc5c40ec05f76066869b33bdac
김덕현 <kdhmny@naver.com>
2022-08-14 22:55:55 +0900

    "[Feat]: 간단한 웹 MVC"

---
+++ start/src/main/java/deok/hellodeok/controller/MemberForm.java
@@ -0,0 +1,17 @@
+package deok.hellodeok.controller;
+
+/**
+ * 이게 참 신기함
+ * 어떻게 자동으로 필드가 매핑이 되는 지 원리가 함금함
+ */
+public class MemberForm {
+    private String name;
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+}
