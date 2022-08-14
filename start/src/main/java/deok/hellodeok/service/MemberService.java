51b4c5a2fe3e8080bb2808fc4fb9c215042d6628
김덕현 <kdhmny@joonggonara.co.kr>
2022-08-14 12:11:25 +0900

    [Feat]: 회원서비스 추가

---
+++ start/src/main/java/deok/hellodeok/service/MemberService.java
@@ -0,0 +1,41 @@
+package deok.hellodeok.service;
+
+import deok.hellodeok.domain.Member;
+import deok.hellodeok.repository.MemberRepository;
+import deok.hellodeok.repository.MemoryMemberRepository;
+
+import java.util.List;
+import java.util.Optional;
+
+public class MemberService {
+
+    private final MemberRepository memberRepository = new MemoryMemberRepository();
+
+    /**
+     * 회원가입
+     */
+    public Long join(Member member) {
+        validateDuplicateMember(member);
+
+        memberRepository.save(member);
+        return member.getId();
+    }
+
+    private void validateDuplicateMember(Member member) {
+        memberRepository.findByName(member.getName())
+                .ifPresent(m -> { // 옵셔널 검증하는 메서드
+                    throw new IllegalStateException("이미 존재하는 회원입니다.");
+                });
+    }
+
+    /**
+     * 전체 회원 조회
+     */
+    public List<Member> findMembers() {
+        return memberRepository.findAll();
+    }
+
+    public Optional<Member> findOne(Long memberId){
+        return memberRepository.findById(memberId);
+    }
+}
